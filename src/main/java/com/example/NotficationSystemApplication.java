75a0f43d62ed7f9d3d01fb307feae6dddbb38e81
ajin033 <ajind033@gmail.com>
2024-12-07 21:54:08 +0530

    Inital project commit

---
+++ src/main/java/com/example/NotficationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotficationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotficationSystemApplication.class, args);
+	}
+
+}
